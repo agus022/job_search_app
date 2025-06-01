a04105b89d9da0e9d21fd216e78c5ede81bd6518
Cristian <21030047_itcelaya.edu.mx>
2025-05-31 18:57:24 -0600

    Problemas arreglados

--- lib/cubit/cubits.dart
+++ lib/cubit/cubits.dart
@@ -1,2 +1,4 @@
+export 'package:job_search_oficial/cubit/category_cubit.dart';
+export 'package:job_search_oficial/cubit/service_cubit.dart';
 export 'package:job_search_oficial/cubit/job_cubit.dart';
 export 'package:job_search_oficial/cubit/user_cubit.dart';
