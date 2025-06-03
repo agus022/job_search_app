939b08aa5271cc3a5cb5e3fd1cbfecad68a7a36b
agus022 <flores.jorge.1j@gmail.com>
2025-06-02 21:57:59 -0600

    login-email-cris

--- lib/screens/login_screen.dart
+++ lib/screens/login_screen.dart
@@ -80,7 +80,7 @@ class _LoginScreenState extends State<LoginScreen> {
                 width: double.infinity,
                 child: ElevatedButton(
                   onPressed: () async {
-                    await userCubit.login('21030761@itcelaya.edu.mx', 'panquecito');
+                    await userCubit.login('21030047@itcelaya.edu.mx', '123456');
                     if (userCubit.state.status == UserStatus.logged) {
                       final user = userCubit.state.user!;
                       final userId = user.id;
